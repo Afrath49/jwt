01bcee2cb4c378fb6bb3a6df4871f0385bf0c039
Afrath49 <afrath4964@gmail.com>
2022-12-17 14:45:51 +0530

    m

---
+++ User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace JWTAuthentication
+{
+    public class User
+    {
+        public string UserName { get; set; } = string.Empty;
+
+        public byte[]? PassswordHash { get; set; }
+
+        public byte[]? PasswordSalt { get; set; }
+    }
+}
---
+++ UserDetails.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace JWTAuthentication
+{
+    public class UserDetails
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
